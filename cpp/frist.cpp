8eb72882ecefd7d1811ef6296506a51c67bdc70c
shrisoundharyaa <shrisoundharyaa@gmail.com>
2024-08-19 22:34:28 +0530

    practice cpp basic

---
+++ cpp/frist.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x , y;
+    cin >> x >> y;
+    cout<< "value of x: " << x << " and y: "<< y;
+}
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     cout<<"hello sound"<<endl;
+//     cout<<"heeloo";
+// }
+
+
+
+
+
+
+
+
+// #include<iostream>
+
+// int main(){
+//     std::cout<<"hello sound"<<"\n";
+//     std::cout<<"heeloo";
+// }
